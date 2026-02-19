bf97a25d6fd0fffffbb38f10463aec65892b5652
Radin Reanula <44552327+RadinReanula@users.noreply.github.com>
2026-02-19 10:16:39 +0530

    feat: implement .env for credentials and switch to cloud database
    - Install vlucas/phpdotenv
    - Create .env and .env.example
    - Update database.php to use environment variables
    - Connect to MongoDB Atlas cloud cluster

--- database.php
+++ database.php
@@ -2,8 +2,11 @@
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/mongo_helpers.php';
 
-$client = new MongoDB\Client("mongodb://localhost:27017");
-$db = $client->e_channeling;
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$client = new MongoDB\Client($_ENV['MONGODB_URI']);
+$db = $client->selectDatabase($_ENV['DB_NAME']);
 
 // Connection successful
 ?>
